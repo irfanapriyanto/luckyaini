411a4613a1eba585d9643d6c192f32f5597a466b
Irfan apriyanto <irfan.apriyanto@kgmedia.id>
2023-07-02 02:32:43 +0700

    [UPD]update design

--- index.php
+++ index.php
@@ -1,11 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lucky & Aini</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Udangan Lucky & Aini</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Simple line icons-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href='https://fonts.googleapis.com/css?family=Beau+Rivage' rel='stylesheet' type="text/css" >
+        <link href='https://fonts.googleapis.com/css?family=Parisienne' rel='stylesheet' type="text/css" >
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Header-->
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h3 class="mb-5 text-secondary">Our Wedding Day</h3>
+                <h1 class="mb-1 text-secondary"><em>Lucky & Nur'aini</em></h1>
+                <h4>Minggu, 9 Juli 2023</h4>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="content-section bg-light" id="about">
+            <div class="container px-4 px-lg-5 text-center">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <p class="lead mb-5">
+                            Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia Menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tentram kepadanya, dan Dia Menjadikan di antaramu rasa kasih dan sayang.
+                        </p>
+                        <p><b>(Qs. Ar-rum 21 )</b></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="content-section bg-primary text-white text-center" id="services">
+            <div class="container px-4 px-lg-5">
+                <div class="content-section-heading">
+                    <p>Dengan memohon rahmat dan ridho Allah SWT yang telah menciptakan Makhluk-Nya secara berpasang-pasangan, In Syaa Allah kami akan menyelenggarakan acara pernikahan :</p>
+                </div>
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-5 col-md-6 mb-5 mb-lg-0">
+                        <img class="img-thumbnail img-fluid" src="assets/img/lucky.png" alt="lucky_profile">
+                        <h4 class="text-secondary"><strong>Lucky Anggasaputra Erstian</strong></h4>
+                        <p><b>Putra Pertama</b></p>
+                        <p class="text-faded mb-0">(Alm.) Setiono & (Almh.) Erlina Syakhriani</p>
+                    </div>
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-5 col-md-6 mb-5 mb-lg-0">
+                        <img class="img-thumbnail2 img-fluid" src="assets/img/aini.png" alt="aini_profile">
+                        <h4 class="text-secondary"><strong>Nuraini Putri Anggraini Marbun</strong></h4>
+                        <p><b>Putra Pertama</b></p>
+                        <p class="text-faded mb-0">(Alm.) Imron Nurdin & Yusnidar</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Callout-->
+        <section class="callout">
+            <div class="container px-4 px-lg-5 text-center">
+                
+                <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-home"></i></span>
+                <h1>- Akad Nikah & Resepsi -</h1>
+                <h3 class="text-secondary">Minggu, 9 Juli 2023</h3>
+                <h4 class="text-dark">10.00 - 16.00</h4>
+                <h4>Jl. Raya Kalibata Gg. Madrasah RT. 03/01 No. 25J, <br> Rawajati-Pancoran Jakarta Selatan (12750)</h4>
+                <a class="btn btn-sm btn-primary me-4" href="https://maps.app.goo.gl/sa2ecygQc4vo96Wb9">Open Maps</a>
+                
+                    
+            </div>
+        </section>
+
+        <!-- Map-->
+        <div class="map" id="contact">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.034167453408!2d106.85711557321608!3d-6.259229761277019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f2548493f917%3A0x66525a25d36f12ce!2sJl.%20Raya%20Kalibata%20Blk.%20Pelangi%20No.28%2C%20RT.10%2FRW.1%2C%20Rawajati%2C%20Kec.%20Pancoran%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2012750!5e0!3m2!1sen!2sid!4v1688236814236!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <!-- Call to Action-->
+        <section class="content-section bg-primary text-white">
+            <div class="container px-4 px-lg-5 text-center">
+                <h2 class="mb-4 text-lebay">Amplop Digital</h2>
+                <p>Doa Restu Anda merupakan karunia yang sangat berarti bagi kami.</p>
+                <p>Dan jika memberi adalah ungkapan tanda kasih Anda, Anda dapat memberi kado secara cashless</p>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card mb-3 text-bg-primary border-primary">
+                            <img src="assets/img/mandiri.png" class="card-img-top img-logos" alt="bank-mandiri">
+                            <div class="card-body">
+                                <h5 class="card-title">1650001958207</h5>
+                                <p class="card-text">An Lucky Anggasaputra Erstian</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card mb-3 text-bg-primary border-primary">
+                            <img src="assets/img/bca.png" class="card-img-top img-logos" alt="bank-bca">
+                            <div class="card-body">
+                                <h5 class="card-title">0845281292</h5>
+                                <p class="card-text">An Nuraini Putri Anggraini Marbun</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container px-4 px-lg-5">
+                <p class="text-muted small mb-0">Copyright &copy; <a href="https://www.linkedin.com/in/irfanapriyanto91/">Irfan Apriyanto</a> 2023</p>
+            </div>
+        </footer>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="assets/scripts.js"></script>
+    </body>
 </html>
